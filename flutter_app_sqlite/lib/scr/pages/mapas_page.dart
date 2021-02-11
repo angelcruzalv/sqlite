a997bdf016a03c96a65ba62dd13f2898cfb22ef4
Angel Cruz (Windows) <37281529+angelcruzalv@users.noreply.github.com>
2021-02-11 17:08:17 -0600

    Code  Optimization
    New widget ScanTiles, fills  mapas and direcciones page

--- flutter_app_sqlite/lib/scr/pages/mapas_page.dart
+++ flutter_app_sqlite/lib/scr/pages/mapas_page.dart
@@ -1,54 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_sqlite/scr/services/scanlist_provider.dart';
+import 'package:flutter_app_sqlite/scr/widgets/scan_tiles.dart';
 import 'package:provider/provider.dart';
 
 class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-    final scans = scanListProvider.scans;
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Dismissible(
-          key: UniqueKey(),
-          background: Container(
-            color: Colors.red,
-            child: Padding(
-              padding: EdgeInsets.only(left: 32.0, right: 12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                  Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          onDismissed: (DismissDirection direction) {
-            Provider.of<ScanListProvider>(context, listen: false)
-                .deleteScanById(scans[index].id);
-          },
-          child: ListTile(
-            leading: Icon(
-              Icons.map,
-              color: Theme.of(context).primaryColor,
-            ),
-            title: Text(scans[index].valor),
-            subtitle: Text(scans[index].id.toString()),
-            trailing: Icon(Icons.keyboard_arrow_down),
-            onTap: () {
-              print(scans[index].id);
-            },
-          ),
-        );
-      },
-    );
+    return ScanTiles(type: 'geo');
   }
 }
---
+++ flutter_app_sqlite/lib/scr/widgets/scan_tiles.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_sqlite/scr/services/scanlist_provider.dart';
+import 'package:provider/provider.dart';
+
+class ScanTiles extends StatelessWidget {
+  final String type;
+
+  const ScanTiles({@required this.type});
+
+  @override
+  Widget build(BuildContext context) {
+    final scanListProvider = Provider.of<ScanListProvider>(context);
+    final scans = scanListProvider.scans;
+
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+          key: UniqueKey(),
+          background: Container(
+            color: Colors.red,
+            child: Padding(
+              padding: EdgeInsets.only(left: 12.0, right: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                  Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ),
+          onDismissed: (DismissDirection direction) {
+            Provider.of<ScanListProvider>(context, listen: false)
+                .deleteScanById(scans[index].id);
+          },
+          child: ListTile(
+            leading: Icon(
+              this.type == 'http' ? Icons.link : Icons.map,
+              color: Theme.of(context).primaryColor,
+            ),
+            title: Text(scans[index].valor),
+            subtitle: Text(scans[index].id.toString()),
+            trailing: Icon(Icons.keyboard_arrow_down),
+            onTap: () {
+              print(scans[index].id);
+            },
+          ),
+        );
+      },
+    );
+  }
+}
